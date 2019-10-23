4eaac9a30f8322c7e259c8c048962efd6f485be1
Janelle Otero <52089302+janelle-otero@users.noreply.github.com>
2019-10-23 00:38:27 -0400

    Add files via upload
    included file

---
+++ distance.cpp
@@ -0,0 +1,70 @@
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+/* Name: Janelle Otero
+Date: 9/30/19
+Section: 10
+Assignment: Homework #3
+Due Date: 10/4/19
+About this project: With this project I will be able to calculate the distance for meters, centimeters, and millimeters in millimeters. After converting those numbers to mm I will showcase the difference.
+All work below was performed by Janelle Otero */
+
+// function with no parameters or values to greet user
+void WelcomeUser(){
+    printf("Welcome to the Converter! \nEnter two distances and they'll be converted to millimeters (mm).\n");
+    return;
+}
+
+// fucntion takes in integer values then calculates how to turn them into millimeters
+int NumMillimeters(int metersinmm, int centimetersinmm, int millimeters){
+    int dist = 0;
+    dist = metersinmm *= 1000;
+    dist += centimetersinmm *= 10;
+    dist += millimeters *= 1;
+    return dist;
+}
+
+int main() {
+
+    //initiliazing welcome function
+    WelcomeUser();
+
+    //initiliazing integers from NumMillimeters function
+    int mt1, cm1, mm1;
+    int mt2, cm2, mm2;
+
+    //asking user to plug in first distances
+
+    cout << "Input first distance..." << endl;
+
+    cout << "Meters:";
+    cin >> mt1;
+    cout << "Centimeters:";
+    cin >> cm1;
+    cout << "Millimeters:";
+    cin >> mm1;
+    int one = NumMillimeters(mt1, cm1, mm1);
+
+    //asking user to plug in second distances
+
+    cout << "\nInput second distance...";
+
+    cout << "\nMeters:";
+    cin >> mt2;
+    cout << "Centimeters:";
+    cin >> cm2;
+    cout << "Millimeters:";
+    cin >> mm2;
+    int two = NumMillimeters(mt2, cm2, mm2);
+
+    //printing the results of the distances for each distance in mm
+    cout << "\nThe first distance is " << one << " mm.";
+    cout << "\nThe second distance is " << two << " mm.";
+
+    //printing the difference between the two results
+    cout << "\nThe difference is " << one - two << " mm.";
+
+    return 0;
+}
